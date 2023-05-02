3d88093a0802d80ab4f41730ec0d45d92238c9a9
teodoradumitr7 <teodoradt19@yahoo.com>
2023-05-02 13:19:31 +0300

    added chain, strategy, observer, flyweight

--- CTS_seminare/seminar_10/chainOfResp/clase/NotificareManager.java
+++ CTS_seminare/seminar_10/chainOfResp/clase/NotificareManager.java
@@ -5,7 +5,7 @@ public class NotificareManager extends Notificator {
 	@Override
 	public void notificaClient(Client client, String mesaj) {
 		// TODO Auto-generated method stub
-				System.out.println("Clientul "+client.getNume()+" nu primeste notificare");
+				System.out.println("Clientul "+client.getNume()+" nu primeste notificare deoarece nu are date");
 			
 		}
 		
